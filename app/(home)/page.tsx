f9cabecc396235c6a3a5820d610a9d631a03c1c3
Rodney Shen <114166609+rodnnnney@users.noreply.github.com>
2025-01-06 22:35:50 -0500

    intial commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function HomePage() {
+  return (
+    <main className="flex flex-1 flex-col justify-center text-center">
+      <h1 className="mb-4 text-2xl font-bold">Carleton Blockchain Docs</h1>
+    </main>
+  );
+}
