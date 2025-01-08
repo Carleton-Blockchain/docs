270a3a8a54e7420e6bca0ea6bc95453f047c0a99
Rodney Shen <114166609+rodnnnney@users.noreply.github.com>
2025-01-08 00:32:30 -0500

    origins page

--- source.config.ts
+++ source.config.ts
@@ -1,7 +1,16 @@
 import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export const { docs, meta } = defineDocs({
   dir: 'content/docs',
 });
 
-export default defineConfig();
+export default defineConfig({
+  mdxOptions: {
+    remarkPlugins: [remarkMath],
+    // Place it at first so that it won't be changed by syntax highlighter
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+  },
+});
+
